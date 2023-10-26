f6e8f0430ad33a3b896306b1094ba63aeaffb89d
Liviu-Gabriel Nedelcu <liviunedelcu10@gmail.com>
2023-10-26 16:07:53 +0300

    Updated Getters and Setters For Player Class

--- Particular-Science-Proj/Particular-Science-Proj/Player.cpp
+++ Particular-Science-Proj/Particular-Science-Proj/Player.cpp
@@ -1,5 +1,15 @@
 #include "Player.h"
 
+void Player::SetIsDrawing()
+{
+    m_isDrawing = true;
+}
+
+void Player::UpdateScore(int16_t newscore)
+{
+    m_personalScore = newscore;
+}
+
 bool Player::GetIsDrawing()
 {
     return m_isDrawing;
@@ -8,4 +18,14 @@ bool Player::GetIsDrawing()
 int16_t Player::GetPersonalScore()
 {
     return m_personalScore;
+}
+
+std::string Player::GetPlayerUsername()
+{
+    return User::GetUsername();
+}
+
+bool Player::GetHasTopScore()
+{
+    return m_hasTopScore;
 }
