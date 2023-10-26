133cabe0038e09e0b8b3200b724d8a1de116854e
RazvanD7 <razvan.dascalu@student.unitbv.ro>
2023-10-26 14:21:19 +0300

    User constructors implementation added

--- Particular-Science-Proj/Particular-Science-Proj/User.cpp
+++ Particular-Science-Proj/Particular-Science-Proj/User.cpp
@@ -1,1 +1,27 @@
 #include "User.h"
+
+User::User(): m_username(""), m_email(""), m_password(""), m_personalBest(0)
+{
+
+}
+
+User::User(const std::string& username, const std::string& email, const std::string& password, uint16_t personalBest): 
+	m_username(username), m_email(email), m_password(password), m_personalBest(personalBest)
+{
+}
+
+User& User::operator=(const User& user)
+{
+	m_username = user.m_username;
+	m_email = user.m_email;
+	m_password = user.m_password;
+	m_personalBest = user.m_personalBest;
+}
+
+User::User(const User& user)
+{
+	m_username = user.m_username;
+	m_email = user.m_email;
+	m_password = user.m_password;
+	m_personalBest = user.m_personalBest;
+}
--- Particular-Science-Proj/Particular-Science-Proj/User.h
+++ Particular-Science-Proj/Particular-Science-Proj/User.h
@@ -3,11 +3,29 @@
 #include <string>
 class User
 {
+public:
+	User();
+	User(const std::string& username, const std::string& email, const std::string& password, uint16_t personalBest);
+	User& operator=(const User& user);
+	User(const User& user);
+
+	std::string GetUsername();
+	std::string GetEmail();
+	std::string GetPassword();
+	//GetGameHistory()
+	uint16_t GetPersonalBest();
+
+	void SetUsername(const std::string& username);
+	void SetEmail(const std::string& email);
+	void SetPassword(const std::string& password);
+	//void SetGameHistory()
+	void SetPersonalBest(uint16_t personalBest);
+
 private:
 	std::string m_username;
 	std::string m_email;
 	std::string m_password;
-	//istoric jocuri
-	uint16_t m_personal_best;
+	//gameHistory
+	uint16_t m_personalBest;
 };
 
