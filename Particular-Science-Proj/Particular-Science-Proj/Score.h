96e970ee7a109fdc95db9e786d0dfba4de8a1c9e
AlexiaMihaela <alexia.chirila@student.unitbv.ro>
2023-10-27 10:37:50 +0300

    Score class: functions declaration added

--- Particular-Science-Proj/Particular-Science-Proj/Score.h
+++ Particular-Science-Proj/Particular-Science-Proj/Score.h
@@ -4,7 +4,25 @@
 class Score
 {
 private:
-	uint16_t timp;
-	std::string dificultate;
+	uint16_t m_timp;
+	std::string m_dificultate;
+
+public:
+	Score();
+
+	Score(uint16_t timp, const std::string& dificultate);
+	
+	Score(const Score& s);
+	
+	Score& operator=(const Score& s);
+	
+	void SetTime(uint16_t timp);
+	
+	void SetDificulty(const std::string& dificultate);
+	
+	uint16_t GetTime();
+
+	std::string GetDificulty();
+	
 };
 
