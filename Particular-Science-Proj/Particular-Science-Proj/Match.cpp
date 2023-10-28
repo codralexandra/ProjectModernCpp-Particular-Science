f4f85b1bd209025f8fb30a61d58927ea48cb9ca3
Codreanu Alexandra <ioana.codreanu@student.unitbv.ro>
2023-10-28 12:17:11 +0300

    Added setters and getters for Match class

--- Particular-Science-Proj/Particular-Science-Proj/Match.cpp
+++ Particular-Science-Proj/Particular-Science-Proj/Match.cpp
@@ -8,3 +8,33 @@ Match::Match(std::vector<Player> players, Difficulty difficulty, std::vector<std
 	m_players{ players }, m_difficulty{ difficulty }, m_words{ words }, m_date{ date }
 {
 }
+
+void Match::setPlayers(std::vector<Player> players)
+{
+	m_players = players;
+}
+
+void Match::setWords(std::vector<std::string> words)
+{
+	m_words = words;
+}
+
+void Match::setDate(std::string date)
+{
+	m_date = date;
+}
+
+std::vector<Player> Match::getPlayers()
+{
+	return m_players;
+}
+
+std::vector<std::string> Match::getWords()
+{
+	return m_words;
+}
+
+std::string Match::getDate()
+{
+	return m_date;
+}
--- Particular-Science-Proj/Particular-Science-Proj/Match.h
+++ Particular-Science-Proj/Particular-Science-Proj/Match.h
@@ -8,12 +8,20 @@ class Match
 public:
 	Match();
 	Match(std::vector<Player> players, Difficulty difficulty, std::vector<std::string> words, std::string date);
+
+	void setPlayers(std::vector<Player> players);
+	void setWords(std::vector<std::string> words);
+	void setDate(std::string date);
+
+	std::vector<Player> getPlayers();
+	std::vector<std::string> getWords();
+	std::string getDate();
 private:
 	std::vector<Player> m_players;
 	Difficulty m_difficulty;
 	static const uint16_t m_rounds = 4;
 	std::vector<std::string> m_words;
-	std::string m_date; //regex check
+	std::string m_date; //regex check or make class
 
 };
 
