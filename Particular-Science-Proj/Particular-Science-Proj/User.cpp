e2e0124fb6e2fce8f0b177cfe5e654cf5a440908
RazvanD7 <razvan.dascalu@student.unitbv.ro>
2023-10-31 21:23:07 +0200

    username deleted from class User

--- Particular-Science-Proj/Particular-Science-Proj/User.cpp
+++ Particular-Science-Proj/Particular-Science-Proj/User.cpp
@@ -1,26 +1,25 @@
 #include "User.h"
 
-User::User(): m_username(""), m_email(""), m_password(""), m_personalBest(0)
+User::User(): m_email(""), m_password(""), m_personalBest(0)
 {
 
 }
 
-User::User(const std::string& username, const std::string& email, const std::string& password, uint16_t personalBest): 
-	m_username(username), m_email(email), m_password(password), m_personalBest(personalBest)
+User::User(const std::string& email, const std::string& password, uint16_t personalBest): 
+	m_email(email), m_password(password), m_personalBest(personalBest)
 {
 }
 
 User& User::operator=(const User& user)
 {
-	m_username = user.m_username;
 	m_email = user.m_email;
 	m_password = user.m_password;
 	m_personalBest = user.m_personalBest;
+	return *this;
 }
 
 User::User(const User& user)
 {
-	m_username = user.m_username;
 	m_email = user.m_email;
 	m_password = user.m_password;
 	m_personalBest = user.m_personalBest;
@@ -28,11 +27,6 @@ User::User(const User& user)
 
 User::~User()
 {
-}
-
-std::string User::GetUsername()
-{
-	return m_username;
 }
 
 std::string User::GetEmail()
@@ -48,11 +42,6 @@ std::string User::GetPassword()
 uint16_t User::GetPersonalBest()
 {
 	return m_personalBest;
-}
-
-void User::SetUsername(const std::string& username)
-{
-	m_username = username;
 }
 
 void User::SetEmail(const std::string& email)
--- Particular-Science-Proj/Particular-Science-Proj/User.h
+++ Particular-Science-Proj/Particular-Science-Proj/User.h
@@ -5,26 +5,23 @@ class User
 {
 public:
 	User();
-	User(const std::string& username, const std::string& email, const std::string& password, uint16_t personalBest);
+	User(const std::string& email, const std::string& password, uint16_t personalBest);
 	User& operator=(const User& user);
 	User(const User& user);
 
 	~User();
 
-	std::string GetUsername();
 	std::string GetEmail();
 	std::string GetPassword();
 	//GetGameHistory()
 	uint16_t GetPersonalBest();
 
-	void SetUsername(const std::string& username);
 	void SetEmail(const std::string& email);
 	void SetPassword(const std::string& password);
 	//void SetGameHistory()
 	void SetPersonalBest(uint16_t personalBest);
 
 private:
-	std::string m_username;
 	std::string m_email;
 	std::string m_password;
 	//gameHistory
