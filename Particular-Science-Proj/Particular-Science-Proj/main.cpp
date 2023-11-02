f8096d99ebd8b6af416a2016a228eb6751491181
AlexiaMihaela <alexia.chirila@student.unitbv.ro>
2023-11-02 21:11:43 +0200

    Function Score for players who are guessing added

--- Particular-Science-Proj/Particular-Science-Proj/main.cpp
+++ Particular-Science-Proj/Particular-Science-Proj/main.cpp
@@ -1,4 +1,26 @@
 //test main for pull, modify as needed
+#include <iostream>
+#include "Player.h"
+void Score_Player_Guessing(Player& p,int timp)
+{
+	if (timp == 60)
+	{
+		p.UpdateScore(p.GetPersonalScore() - 100);
+	}
+	else if (timp < 30)
+	{
+		p.UpdateScore(p.GetPersonalScore() + 100);
+	}
+	else
+	{
+		int s = (60 - timp) * 100 / 30;
+		p.UpdateScore(p.GetPersonalScore() + s);
+	}
+}
+
+
+
+
 
 int main() {
 	return 0;
