ac88c8936f5ce7db1fd64bee018f5147aa259752
AlexiaMihaela <alexia.chirila@student.unitbv.ro>
2023-11-02 21:34:58 +0200

    Function Score added for player who is drawing

--- Particular-Science-Proj/Particular-Science-Proj/main.cpp
+++ Particular-Science-Proj/Particular-Science-Proj/main.cpp
@@ -1,5 +1,6 @@
 //test main for pull, modify as needed
 #include <iostream>
+#include <vector>
 #include "Player.h"
 void Score_Player_Guessing(Player& p,int timp)
 {
@@ -18,7 +19,25 @@ void Score_Player_Guessing(Player& p,int timp)
 	}
 }
 
+void Score_Player_Drawing(Player& p, std::vector<Player>players_guessing)
+{
+	float average_time;
+	int sum_times;
 
+	for (int i = 0; i < players_guessing.size(); i++)
+	{
+		sum_times = players_guessing[i].GetTimeGuessed() + sum_times;
+	}
+
+	if (sum_times == 60 * players_guessing.size())
+		p.UpdateScore(p.GetPersonalScore() - 100);
+	else
+	{
+		average_time = sum_times / players_guessing.size();
+		p.UpdateScore(p.GetPersonalScore() + (60 - average_time) * 100 / 60);
+
+	}
+}
 
 
 
