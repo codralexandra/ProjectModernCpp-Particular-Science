05f54adafb584450e1de0d6a5e5eff82a9962a9d
AlexiaMihaela <alexia.chirila@student.unitbv.ro>
2023-11-11 15:09:24 +0200

    SubRound class added with setter and getter and word function for print

---
+++ Particular-Science-Proj/Particular-Science-Proj/SubRound.cpp
@@ -0,0 +1,29 @@
+#include "SubRound.h"
+
+SubRound::SubRound()
+{
+}
+
+void SubRound::SetWord(const std::string& cuv)
+{
+	this->word = cuv;
+}
+
+const std::string SubRound::GetWord() const
+{
+	return this->word;
+}
+
+void SubRound::StartSubRound()
+{
+
+}
+
+void SubRound::PrintWordToGuess()
+{
+	for (int i = 0; i < this->word.size(); i++)
+	{
+		std::cout << "_ ";
+	}
+	std::cout << std::endl;
+}
---
+++ Particular-Science-Proj/Particular-Science-Proj/SubRound.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+#include "Round.h"
+#include <iostream>
+class SubRound:public Round
+{
+	SubRound();
+	void SetWord(const std::string& cuv);
+	const std::string GetWord()const;
+	void StartSubRound();
+	void PrintWordToGuess();
+
+private:
+	std::string word;
+};
+
