c82053187fe822d5072221d5cab5bab705a931ac
Codreanu Alexandra <ioana.codreanu@student.unitbv.ro>
2023-11-14 15:23:28 +0200

    Added Client project

---
+++ Skribbl App/Client/HTTPClient/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <numeric>
+#include <sstream>
+#include <regex>
+#include <string>
+
+#include <cpr/cpr.h>
+#include <crow.h>
+
+// Cerinte client:
+// 1. Se vor afisa toate produsele
+// 2. Se citesc de la tastatura id produs si cantitatea si se vor adauga in cos (prin 2 modalitati)
+// 3. Se afiseaza cosul de cumparaturi si se afiseaza pretul total
+
+
+int  main()
+{
+    std::cout << "I installed cpr. Now, my server is the limit!:(\n";
+
+    return 0;
+}
