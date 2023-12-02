5e1221114e2abb24d5e5753a4a89006ca2532426
Liviu-Gabriel Nedelcu <liviunedelcu10@gmail.com>
2023-12-02 18:20:53 +0200

    QSoundEffect MainMenu song added in main

--- Skribbl App/ClientGUI/main.cpp
+++ Skribbl App/ClientGUI/main.cpp
@@ -1,9 +1,14 @@
 #include "clientgui.h"
 #include <QtWidgets/QApplication>
-
+#include <qsoundeffect.h>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QSoundEffect* sound = new QSoundEffect;
+    sound->setVolume(10);
+    sound->setSource(QUrl::fromLocalFile("E:\Scoala\Facultate\An2\ModernC++\Proiect\Coding\ProjectModernCpp-Particular-Science\Skribbl App\ClientGUI\Sounds\MainMenu.mp3"));
+    sound->setLoopCount(QSoundEffect::Infinite);
+    sound->play();
     ClientGUI w;
     w.show();
     return a.exec();
