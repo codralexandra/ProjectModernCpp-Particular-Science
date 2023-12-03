c6b5f15f3b59d420d6a0209b746a46c4ade5bbe2
AlexiaMihaela <alexia.chirila@student.unitbv.ro>
2023-12-03 11:27:00 +0200

    Populate Database function for words

--- Skribbl App/Server/HTTPServer/gamedata.cpp
+++ Skribbl App/Server/HTTPServer/gamedata.cpp
@@ -1,1 +1,16 @@
 #include "gamedata.h"
+
+void populateStorage(Storage& storage)
+{
+	std::ifstream in("Words.input");
+	std::string word, dif;
+	std::vector<Word2> cuvinte;
+	while (!in.eof())
+	{
+		in >> word >> dif;
+		Word2 w{ -1,word,dif };
+		cuvinte.push_back(w);
+	}
+	storage.insert_range(cuvinte.begin(), cuvinte.end());
+	
+}
