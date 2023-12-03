593245969aa7e05e6da4a7f5a36185f6c96493c0
RazvanD7 <147138179+RazvanD7@users.noreply.github.com>
2023-12-03 09:40:05 +0200

    added login route

--- Skribbl App/Server/HTTPServer/routing.cpp
+++ Skribbl App/Server/HTTPServer/routing.cpp
@@ -7,6 +7,8 @@ void Routing::Run()
 	CROW_ROUTE(m_app, "/")([]() {
 		return "This is an example app of crow and sql-orm";
 		});
+
+
     CROW_ROUTE(m_app, "/register")
         .methods("POST"_method)
         ([](const crow::request& req) {
@@ -20,7 +22,29 @@ void Routing::Run()
             //add to database
         }
         return crow::response(200);;
-            });
+        });
+
+    CROW_ROUTE(m_app, "/login")
+        .methods("POST"_method)
+        ([](const crow::request& req) {
+        auto bodyArgs = parseUrlArgs(req.body);
+        auto end = bodyArgs.end();
+        auto usernameIter = bodyArgs.find("username");
+        auto passwordIter = bodyArgs.find("password");
+        if (usernameIter != end && passwordIter != end) {
+           // bool isAuthenticated = checkUser(usernameIter->second, passwordIter->second);
+            bool isAuthenticated = true;
+            if (isAuthenticated) {
+                return crow::response(200, "Login successful");
+            }
+            else {
+                return crow::response(401, "Unauthorized");
+            }
+        }
+        else {
+            return crow::response(400, "Bad Request");
+        }
+      });
 
 
 	m_app.port(18080).multithreaded().run();
