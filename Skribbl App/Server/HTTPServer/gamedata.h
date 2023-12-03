2109c76b01b11a3556127b964e43dc68ec108c46
AlexiaMihaela <alexia.chirila@student.unitbv.ro>
2023-12-03 11:11:21 +0200

    Struct user added and table User2 created in Database

--- Skribbl App/Server/HTTPServer/gamedata.h
+++ Skribbl App/Server/HTTPServer/gamedata.h
@@ -7,27 +7,32 @@ namespace sql = sqlite_orm;
 using namespace Scribble;
 import utils;
 
-struct balbla {
-	int a, b;
+struct Word2 {
+	uint16_t m_id;
+	std::string m_value;
+	std::string m_difficulty;
+};
+
+struct User2 {
+	std::string m_email;
+	std::string m_password;
 };
 
 inline auto createStorage(const std::string& filename)
 {
 	return sql::make_storage(
 		filename,
-		//sql::make_table(
-		//	"Word",
-		//	sql::make_column("id", &Word::getId(), sql::primary_key().autoincrement()),
-		//	sql::make_column("name", &Word::getValue()),
-		//	sql::make_column("difficulty", &Word::getDifficulty())
-		//	//restul de membrii din word
-		//)
 		sql::make_table(
-			"Word",
-			sql::make_column("id", &balbla::a, sql::primary_key().autoincrement()),
-			sql::make_column("name", &balbla::b)
-			)
-		
+			"Word2",
+			sql::make_column("id", &Word2::m_id, sql::primary_key().autoincrement()),
+			sql::make_column("name", &Word2::m_value),
+			sql::make_column("difficulty", &Word2::m_difficulty)
+		),
+		sql::make_table(
+			"User2",
+			sql::make_column("username", &User2::m_email, sql::primary_key().autoincrement()),
+			sql::make_column("password", &User2::m_password)
+		)
 		
 	);
 }
