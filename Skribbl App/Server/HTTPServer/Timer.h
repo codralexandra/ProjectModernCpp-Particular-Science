6037f7da9fefca84d7f00808495c12bf7cc10ddc
AlexiaMihaela <alexia.chirila@student.unitbv.ro>
2023-12-05 14:46:01 +0200

    Class Timer added

---
+++ Skribbl App/Server/HTTPServer/Timer.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include <chrono>
+#include <ctime>
+#include <cmath>
+#include <thread>
+class Timer
+{
+public:
+	void start();
+	void stop();
+	double elapsedMilliseconds();
+	double elapsedSeconds();
+
+private:
+	std::chrono::time_point<std::chrono::system_clock> m_StartTime;
+	std::chrono::time_point<std::chrono::system_clock> m_EndTime;
+	bool  m_bRunning = false;
+};
+
