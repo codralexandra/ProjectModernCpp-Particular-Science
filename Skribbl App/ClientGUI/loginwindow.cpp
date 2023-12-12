8d73d3d0ebfd551870ac592e21355ae6b761a5d1
Codreanu Alexandra <ioana.codreanu@student.unitbv.ro>
2023-12-12 21:56:25 +0200

    Added functionality for log in button (connection to server to be added)

--- Skribbl App/ClientGUI/loginwindow.cpp
+++ Skribbl App/ClientGUI/loginwindow.cpp
@@ -5,10 +5,29 @@ LoginWindow::LoginWindow(QWidget *parent)
 {
 	ui.setupUi(this);
 	connect(ui.backButton, &QPushButton::clicked, this, &LoginWindow::on_backButton_clicked);
+	connect(ui.loginButton, &QPushButton::clicked, this, &LoginWindow::on_loginButton_clicked);
 }
 
 LoginWindow::~LoginWindow()
 {}
+
+void LoginWindow::on_loginButton_clicked()
+{
+	//pack user inputs
+	std::string email = "";
+	std::string username{ ui.usernameInput->text().toUtf8() };
+	std::string password{ ui.passwordInput->text().toUtf8() };
+
+	crow::json::wvalue jsonPayload;
+	jsonPayload["email"] = email;			//log in with username, email is not important
+	jsonPayload["username"] = username;
+	jsonPayload["password"] = password;
+	std::string jsonString = jsonPayload.dump();
+	//send to server to interogate user db
+	//if server response is ok, open main window
+	//else set qLabel to wrong username/password 
+	ui.loginStateLabel->setText("Username/ Password combination is wrong.");
+}
 
 void LoginWindow::on_backButton_clicked()
 {
--- Skribbl App/ClientGUI/loginwindow.h
+++ Skribbl App/ClientGUI/loginwindow.h
@@ -2,6 +2,8 @@
 
 #include <QWidget>
 #include "ui_loginwindow.h"
+#include <cpr/cpr.h>
+#include <crow.h>
 
 class LoginWindow : public QWidget
 {
@@ -11,6 +13,7 @@ public:
 	LoginWindow(QWidget *parent = nullptr);
 	~LoginWindow();
 public slots:
+	void on_loginButton_clicked();
 	void on_backButton_clicked();
 
 private:
