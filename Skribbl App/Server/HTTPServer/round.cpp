594c306c7306375b2a37fbb1c94689218add01fc
HookMyHooks <liviunedelcu10@gmail.com>
2023-12-14 18:14:51 +0200

    Uncommented round working

--- Skribbl App/Server/HTTPServer/round.cpp
+++ Skribbl App/Server/HTTPServer/round.cpp
@@ -1,5 +1,5 @@
 #include "Round.h"
-
+#include <iostream>
 Round::Round()
 {
 	
@@ -21,47 +21,47 @@ Round::Round()
 //	//end round
 //}
 
-//void Round::Score_Player_Drawing(Scribble::Player& p, std::vector<Scribble::Player>players_guessing)
-//{
-//	std::cout << "Score Drawing apelata\n";
-//	float average_time;
-//	int sum_times = 0;
-//
-//	for (int i = 0; i < players_guessing.size(); i++)
-//	{
-//		if (players_guessing[i].GetIsDrawer() == false)
-//			sum_times = players_guessing[i].GetTimeGuessed() + sum_times;
-//	}
-//
-//	if (sum_times == 60 * players_guessing.size())
-//		p.SetScore(p.GetPersonalScore() - 100);
-//	else
-//	{
-//		average_time = sum_times / players_guessing.size();
-//		p.SetScore(p.GetPersonalScore() + (60 - average_time) * 100 / 60);
-//
-//	}
-//}
+void Round::Score_Player_Drawing(Scribble::Player& p, std::vector<Scribble::Player>players_guessing)
+{
+	std::cout << "Score Drawing apelata\n";
+	float average_time;
+	int sum_times = 0;
 
-//void Round::Score_Player_Guessing(std::vector<Scribble::Player>& p)
-//{
-//	for (auto player : p)
-//	{
-//		if (player.GetIsDrawer() == false)
-//		{
-//			if (p.GetTimeGuessed() == 60)
-//			{
-//				p.SetScore(p.GetPersonalScore() - 50);
-//			}
-//			else if (p.GetTimeGuessed() < 30)
-//			{
-//				p.SetScore(p.GetPersonalScore() + 100);
-//			}
-//			else
-//			{
-//				int s = (60 - timp) * 100 / 30;
-//				p.SetScore(p.GetPersonalScore() + s);
-//			}
-//		}
-//	}
-//}
+	for (int i = 0; i < players_guessing.size(); i++)
+	{
+		if (players_guessing[i].GetIsDrawer() == false)
+			sum_times = players_guessing[i].GetTimeGuessed() + sum_times;
+	}
+
+	if (sum_times == 60 * players_guessing.size())
+		p.SetScore(p.GetPersonalScore() - 100);
+	else
+	{
+		average_time = sum_times / players_guessing.size();
+		p.SetScore(p.GetPersonalScore() + (60 - average_time) * 100 / 60);
+
+	}
+}
+
+void Round::Score_Player_Guessing(std::vector<Scribble::Player>& p)
+{
+	for (auto player : p)
+	{
+		if (player.GetIsDrawer() == false)
+		{
+			if (player.GetTimeGuessed() == 60)
+			{
+				player.SetScore(player.GetPersonalScore() - 50);
+			}
+			else if (player.GetTimeGuessed() < 30)
+			{
+				player.SetScore(player.GetPersonalScore() + 100);
+			}
+			else
+			{
+				int s = (60 - player.GetTimeGuessed()) * 100 / 30;
+				player.SetScore(player.GetPersonalScore() + s);
+			}
+		}
+	}
+}
--- Skribbl App/Server/HTTPServer/round.h
+++ Skribbl App/Server/HTTPServer/round.h
@@ -1,5 +1,6 @@
 #include "SubRound.h"
 import player;
+#include <vector>
 //import game;
 
 class Round
@@ -8,8 +9,8 @@ public:
 	Round();
 	
 	//void StartRound(Scribble::Game& g);
-	//void Score_Player_Drawing(Scribble::Player& p, std::vector<Scribble::Player>players_guessing);
-	//void Score_Player_Guessing(std::vector<Scribble::Player>& p);
+	void Score_Player_Drawing(Scribble::Player& p, std::vector<Scribble::Player>players_guessing);
+	void Score_Player_Guessing(std::vector<Scribble::Player>& p);
 
 private:
 
