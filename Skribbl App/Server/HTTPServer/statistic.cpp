9c519c81cdca9d8bc992db392534089458fa52a7
AlexiaMihaela <alexia.chirila@student.unitbv.ro>
2023-12-24 23:54:25 +0200

    Setter and getter added

--- Skribbl App/Server/HTTPServer/statistic.cpp
+++ Skribbl App/Server/HTTPServer/statistic.cpp
@@ -2,8 +2,29 @@
 
 Statistic::Statistic()
 {
+	//empty
 }
 
 Statistic::Statistic(const std::string& username, uint16_t score):m_score(score),m_username(username)
 {
+}
+
+void Statistic::SetUsername(const std::string& username)
+{
+	this->m_username = username;
+}
+
+void Statistic::SetScore(uint16_t& score)
+{
+	this->m_score = score;
+}
+
+std::string Statistic::GetUsername() const
+{
+	return m_username;
+}
+
+uint16_t Statistic::GetScore() const
+{
+	return m_score;
 }
--- Skribbl App/Server/HTTPServer/statistic.h
+++ Skribbl App/Server/HTTPServer/statistic.h
@@ -5,6 +5,16 @@ class Statistic
 public:
 	Statistic();
 	Statistic(const std::string& username, uint16_t score);
+	~Statistic() = default;
+
+	//setter
+	void SetUsername(const std::string& username);
+	void SetScore(uint16_t& score);
+	
+	//getter
+	std::string GetUsername() const;
+	uint16_t GetScore()const;
+
 private:
 	std::string m_username;
 	uint16_t m_score;
